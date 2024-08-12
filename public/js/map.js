d9ff7f2bee4f7ab341e261b985c6ee47d78edb42
Prathamesh-Darekar <darekarprathamesh2004@gmail.com>
2024-08-12 20:39:06 +0530

    PROJECT DEPLOYED

--- public/js/map.js
+++ public/js/map.js
@@ -2,19 +2,6 @@ let mapContainer = document.createElement("div");
 mapContainer.id = "map";
 document.getElementById("map-container").appendChild(mapContainer);
 // DISPLAYING MAP
-// const map = new maplibregl.Map({
-//   container: "map",
-//   style:
-//     "https://api.maptiler.com/maps/streets/style.json?key=houAlUeGpISvpnNSbo2D",
-//   center: [longitude, latitude], // longitude,latitude
-//   zoom: 6,
-// });
-// const marker = new maplibregl.Marker({
-//   scale: 0.7,
-//   color: "#FE424D",
-// })
-//   .setLngLat([longitude, latitude])
-//   .addTo(map);
 var map = L.map("map").setView([latitude, longitude], 10);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
